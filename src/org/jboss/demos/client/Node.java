220f0ef2191e5657ec79ed6f04879d17802efa56
yyang <yyang@redhat.com>
2012-11-12 16:33:57 +0800

    Minor fix

--- src/org/jboss/demos/client/Node.java
+++ src/org/jboss/demos/client/Node.java
@@ -9,7 +9,8 @@ import com.google.gwt.dom.client.ImageElement;
  * @create 11/12/12 10:11 AM
  */
 public class Node {
-    private Vector position, goal;
+
+    private Vector position;
 
     private ImageElement image;
 
@@ -17,21 +18,11 @@ public class Node {
 
     public Node(ImageElement image) {
         position = new Vector(0, 0);
-        goal = new Vector(0, 0);
         this.image = image;
     }
 
     public void setPosition(double x, double y) {
         this.position.set(x, y);
-    }
-
-    public void setGoal(double x, double y){
-        this.goal.x = x;
-        this.goal.y = y;
-    }
-
-    public void update() {
-        position.set(goal);
     }
 
     public void draw(Context2d context) {
